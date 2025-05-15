f98188805b73de0640947ec09d81dc0ae4d4644d
Sissi <670408085@qq.com>
2025-05-15 13:14:27 +0800

    git commit -m "Modify training for finetuning with unfreeze, decoder, embed, head"

--- dust3r/training.py
+++ dust3r/training.py
@@ -300,9 +300,9 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     def unfreeze_layers(model):
         # Unfreeze downstream heads
         for param in model.downstream_head1.parameters():
-            param.requires_grad = False
+            param.requires_grad = True
         for param in model.downstream_head2.parameters():
-            param.requires_grad = False
+            param.requires_grad = True
 
         # Unfreeze decoder_embed -  -
         for param in model.decoder_embed.parameters():
