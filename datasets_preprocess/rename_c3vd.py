a0f3d54d348adf7ea98fe0d0bd7bf41c8ef0cf84
Sissi <670408085@qq.com>
2025-06-24 11:38:39 +0800

    git commit -m "finetune for C3VD"

--- datasets_preprocess/rename_c3vd.py
+++ datasets_preprocess/rename_c3vd.py
@@ -2,12 +2,23 @@ import shutil
 import os
 import glob
 
-datadir = "/data_new/luxiaoxi/dataset/medical_slam/C3VD_undistorted/cecum_t1_a_under_review/images"
-imgs_name = sorted(glob.glob(os.path.join(datadir, "*_color.png")), key=lambda x:int(x.split('/')[-1].split('_')[0]))
+# datadir = "/data_new/luxiaoxi/dataset/medical_slam/C3VD_undistorted/cecum_t1_a_under_review/images"
+# imgs_name = sorted(glob.glob(os.path.join(datadir, "*_color.png")), key=lambda x:int(x.split('/')[-1].split('_')[0]))
+#
+# for idx, img_name in enumerate(imgs_name):
+#     num = int(img_name.split('/')[-1].split('_')[0])
+#     old_name = img_name.split('/')[-1]
+#     new_name = "%04d_color.png"%num
+#     new_img_name = img_name.replace(old_name, new_name)
+#     os.rename(img_name, new_img_name)
+
+datadir = "/data_new/luxiaoxi/dataset/medical_slam/C3VD_undistorted/desc_t4_a_p2_under_review/depths"
+imgs_name = sorted(glob.glob(os.path.join(datadir, "*.tiff")))
 
 for idx, img_name in enumerate(imgs_name):
-    num = int(img_name.split('/')[-1].split('_')[0])
+    num = int(img_name.split('/')[-1].split('.')[0])
     old_name = img_name.split('/')[-1]
-    new_name = "%04d_color.png"%num
+    # new_name = "%04d_color.png"%num
+    new_name = "%04d_depth.tiff"%num
     new_img_name = img_name.replace(old_name, new_name)
     os.rename(img_name, new_img_name)
