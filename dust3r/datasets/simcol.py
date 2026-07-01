0c24018e42eee99d7242146d45eefd7af1255696
Sissi <670408085@qq.com>
2026-07-01 16:03:32 +0800

    git commit -m "rank=8, alpha=16, window_length=8"

--- dust3r/datasets/simcol.py
+++ dust3r/datasets/simcol.py
@@ -50,16 +50,16 @@ class SyntheticColon(BaseStereoViewDataset):
 
         # for each scene, we have 100 images ==> 360 degrees (so 25 frames ~= 90 degrees)
         # we prepare all combinations such that i-j = +/- [5, 10, .., 90] degrees
-        # self.combinations = [(i, j)
-        #                      for i, j in itertools.combinations(range(60), 2)
-        #                      if 0 < abs(i - j) <= 10 and abs(i - j) % 3 == 0]
+        self.combinations = [(i, j)
+                             for i, j in itertools.combinations(range(60), 2)
+                             if 0 < abs(i - j) <= 10 and abs(i - j) % 5 == 0]
         # self.combinations = [(i, j)
         #                      for i, j in itertools.combinations(range(10), 2)
         # #                      if 0 < abs(i - j) <= 10 and abs(i - j) % 2 == 0]
-        self.combinations = [(i, i + k)
-                             for i in range(1200)
-                             for k in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-                             if i + k < 1200]
+        # self.combinations = [(i, i + k)
+        #                      for i in range(1200)
+        #                      for k in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        #                      if i + k < 1200]
 
         self.invalidate = {scene: {} for scene in self.scene_list}
 
